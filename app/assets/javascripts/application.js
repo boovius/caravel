6bc4f44c01acb02a09f51bb2b192ca9e764eb141
Joshua Book <joshua.book@gmail.com>
2014-03-15 11:47:11 -0700

    configure angular module

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,6 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
-//= require turbolinks
+//= require ../../../vendor/assets/javascripts
+//= require config
 //= require_tree .
---
+++ app/assets/javascripts/config.js
@@ -0,0 +1,30 @@
+//= require_self
+//= require_tree ./directives
+//= require_tree ./controllers
+//= require_tree ./services
+//= require_tree ./filters
+
+AppChallenge = angular.module('AppChallenge', [
+  'ngRoute',
+  'ngResource',
+  'smartTable.table',
+  'ui.bootstrap'
+])
+
+.config(['$httpProvider', '$routeProvider', '$locationProvider', function($httpProvider, $routeProvider, $locationProvider) {
+
+}])
+
+// Repurposed function to convert camelCase to snake_case
+// https://github.com/nathan7/snakeize
+angular.snakeize = function(obj) {
+  if (!obj || typeof obj !== 'object' || obj instanceof Date === true) return obj;
+  if (Array.isArray(obj)) return obj.map(angular.snakeize);
+  return Object.keys(obj).reduce(function (acc, key) {
+      var camel = key[0].toLowerCase() + key.slice(1).replace(/([A-Z]+)/g, function (m, x) {
+          return '_' + x.toLowerCase();
+      });
+      acc[camel] = angular.snakeize(obj[key]);
+      return acc;
+  }, {});
+}
